f909ef9509416dab973a02996793185b3824afd8
Tangming <infly123@sina.com>
2018-08-09 11:23:53 +0800

    create project

---
+++ src/main/java/com/tm/codemapping/CodemappingApplication.java
@@ -0,0 +1,12 @@
+package com.tm.codemapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemappingApplication.class, args);
+	}
+}

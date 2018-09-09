b3497975f87b12e092b4df703f0d9b239c7a022d
Tangming <infly123@sina.com>
2018-09-09 22:40:24 +0800

    update

--- src/main/java/com/tm/codemapping/Application.java
+++ src/main/java/com/tm/codemapping/Application.java
@@ -3,9 +3,11 @@ package com.tm.codemapping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @MapperScan("com.tm.codemapping.mapper")
+@EnableWebSecurity 
 public class Application {
 
 	public static void main(String[] args) {

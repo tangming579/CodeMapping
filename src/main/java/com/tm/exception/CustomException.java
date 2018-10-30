f19bf69e421cbb2d3df394eddb481a27173d22e8
Tangming <infly123@sina.com>
2018-10-30 20:30:50 +0800

    update

--- src/main/java/com/tm/exception/CustomException.java
+++ src/main/java/com/tm/exception/CustomException.java
@@ -1,5 +1,10 @@
 package com.tm.exception;
 
-public class CustomException {
+public class CustomException extends Exception {
+	
+	private static final long serialVersionUID = -3608667856397125671L;
 
+    public CustomException(String message) {
+        super(message);
+    }
 }

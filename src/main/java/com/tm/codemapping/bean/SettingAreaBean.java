a449fe5fc049834928b2b668542310266a75bbe1
Tangming <infly123@sina.com>
2018-11-03 13:45:46 +0800

    update

---
+++ src/main/java/com/tm/codemapping/bean/SettingAreaBean.java
@@ -0,0 +1,48 @@
+package com.tm.codemapping.bean;
+
+public class SettingAreaBean {
+
+	private String id;
+	private String name;
+	private String floorId;
+	private String buildingId;
+	private String number;
+	private String remark;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFloorId() {
+		return floorId;
+	}
+	public void setFloorId(String floorId) {
+		this.floorId = floorId;
+	}
+	public String getBuildingId() {
+		return buildingId;
+	}
+	public void setBuildingId(String buildingId) {
+		this.buildingId = buildingId;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+}
---
+++ src/main/java/com/tm/codemapping/bean/SettingFloorBean.java
@@ -0,0 +1,26 @@
+package com.tm.codemapping.bean;
+
+public class SettingFloorBean {
+	
+	private String id;
+	private String name;
+	private String buildingId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBuildingId() {
+		return buildingId;
+	}
+	public void setBuildingId(String buildingId) {
+		this.buildingId = buildingId;
+	}
+}
